801c60d536785e99ece7f15e3c10a017d43ddfea
Aiden McHugh <Apmchugh@uvm.edu>
2024-04-20 13:58:45 -0400

    Update mess_around_file_IR_int_w_t-k_for_clock.ino

--- mess_around_file_IR_int_w_t-k_for_clock/mess_around_file_IR_int_w_t-k_for_clock.ino
+++ mess_around_file_IR_int_w_t-k_for_clock/mess_around_file_IR_int_w_t-k_for_clock.ino
@@ -10,8 +10,8 @@ DIYables_IRcontroller_21 irController(IR_RECEIVER_PIN, 200); // debounce time is
 int pressed = 0;
 String number_pressed;
 int mode, mute = 0;
-int hours, minutes, seconds;
-int hours1, minutes1;
+int hours, minutes, seconds; // Global Variables for Hours, Minutes and seconds
+int hours1, minutes1; 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 unsigned long previousMillis;
@@ -25,7 +25,7 @@ const long interval1 = 10;  // interval at which to update the servo (in millise
 int pos = 0;  // current position of the servo
 int direction = 1;  // direction of rotation (1 for increasing, -1 for decreasing)
 
-void IRController() {
+void IRController() { //Function for the IR controller used from libary.
   Key21 key = irController.getKey();
   if (key != Key21::NONE) {
 
@@ -154,7 +154,7 @@ void IRController() {
     }
   }
 }
-void ServoControl() {
+void ServoControl() { // Servo Control function, rotates the servo in 180 degree increments. 
     // Servo Control
     unsigned long currentMillis1 = millis();  // get the current time
 
@@ -180,14 +180,14 @@ void ServoControl() {
 
 void setup() {
   Serial.begin(9600);
-  irController.begin();
+  irController.begin(); //intializes the IR controller
   Serial.setTimeout(100);  // 100ms timeout for serial input
-  Serial.println("Set Time in hh:mm:ss format");
+  Serial.println("Set Time in hh:mm:ss format"); //prints to the serial monitor 
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
   lcd.print("Set time in");
   lcd.setCursor(0, 1);
-  lcd.print("hh:mm:ss format");
+  lcd.print("hh:mm:ss format"); // formatting the 
   delay(500);
   lcd.clear();
   pinMode(52, OUTPUT); // defining buzzer signal pins
@@ -209,16 +209,16 @@ void loop() {
     // then it stays synchronized.
     previousMillis += interval;  // increment with 1 second
 
-    seconds += 1;
-    if (seconds >= 60) {
+    seconds += 1; //Adding a second everytime the interval tracks that a second has occured. This is so there is no problems if there is a delay
+    if (seconds >= 60) { //If seconds get to 60 reset seconds to zero and increment minutes by 1.
       seconds = 0;
       minutes += 1;
     }
-    if (minutes >= 60) {
+    if (minutes >= 60) { //If minutes = 60 reset them to 0. Increase hours by 1.
       minutes = 0;
       hours += 1;
     }
-    if (hours >= 24) {
+    if (hours >= 24) { //If hours = 24 reset them to zero. 
       hours = 0;
     }
 
@@ -269,47 +269,47 @@ void loop() {
     }
     //Serial.println();
   }
-  ServoControl();
-  IRController();
+  ServoControl(); // Call the servo control function
+  IRController(); // Call the IR control function 
+//setting the alarm
+  if (pressed == 1) { // If a button is pressed on the IR remote pressed == 1. 
 
-  if (pressed == 1) {
-
-    while (mode == 1) {
+    while (mode == 1) { // if mode button is pressed join the while loop.
       lcd.setCursor(0, 0);
-      lcd.println("Set Alarm hhmm");
-      IRController();
-      if (pressed == 1) {
-        pressed = 0;
-        count += 1;
+      lcd.println("Set Alarm hhmm"); // print set alarm in format hhmm
+      IRController(); 
+      if (pressed == 1) { // if pressed == 1
+        pressed = 0; // reset pressed = 0
+        count += 1; // increases the count by 1
         Serial.print("alarm count : ");
-        Serial.println(count);
+        Serial.println(count); 
 
-        if (count == 2) {
-          h1 = number_pressed;
+        if (count == 2) { // when the count = 2
+          h1 = number_pressed; // h1 = the first digit for the hours = the number pressed 
         }
 
-        if (count == 3) {
-          h2 = number_pressed;
-          h3 = (String(h1) + String(h2)).toInt();
-          h1 = "";
-          h2 = "";
+        if (count == 3) { // when the count = 3
+          h2 = number_pressed; // h2 = the second digit for hours = the number pressed
+          h3 = (String(h1) + String(h2)).toInt(); // adds the two number inputed as strings and convert them to an integer. 
+          h1 = ""; // Resets h1 to an empty string.
+          h2 = ""; // Resets h2 to an empty string.
         }
 
-        if (count == 4) {
-          m1 = number_pressed;
+        if (count == 4) { // when count = 4
+          m1 = number_pressed; // m1 = the number pressed. Stores as a varible 
         }
 
-        if (count == 5) {
-          m2 = number_pressed;
-          m3 = (String(m1) + String(m2)).toInt();
-          m1 = "";
-          m2 = "";
-          hours1 = constrain(h3, 0, 23);
-          minutes1 = constrain(m3, 0, 59);
+        if (count == 5) { // when count = 5
+          m2 = number_pressed; // m2 = the number pressed. Stores as a varible
+          m3 = (String(m1) + String(m2)).toInt(); // adds the two number inputed as strings and convert them to an integer
+          m1 = ""; // resets to an empty string
+          m2 = ""; // resets to an empty string
+          hours1 = constrain(h3, 0, 23); // constrains hours through 0-23. Reformats if out of constraints
+          minutes1 = constrain(m3, 0, 59); // constrains minutes through 0-23. Reformats if out of constraints
           lcd.setCursor(0, 0);
           lcd.print("Alarm Set");
-          count = 0;
-          pressed = 0;
+          count = 0; // reset count to zero
+          pressed = 0; // reset pressed to zero
           Serial.println();
           Serial.print("alarm hours : ");
           Serial.println(hours1);
@@ -319,46 +319,46 @@ void loop() {
         }
       }
     }
-  
+  // setting the clock time
     count1 += 1;
     Serial.print("set time count : ");
     Serial.println(count1);
 
-    if (count1 == 1) {
-    h4 = number_pressed;
+    if (count1 == 1) { // when a number is pressed
+    h4 = number_pressed; // set the hour 4 variable to number pressed
     }
     
-    if (count1 == 2) {
-      h5 = number_pressed;
-      h6 = (String(h4) + String(h5)).toInt();
-      h4 = "";
-      h5 = "";
+    if (count1 == 2) { // when a number pressed = 2
+      h5 = number_pressed; // set the hour 5 variable to number pressed
+      h6 = (String(h4) + String(h5)).toInt(); // adds the two number inputed as strings and convert them to an integer. 
+      h4 = ""; // reset h4 to an empty string
+      h5 = ""; // reset h5 to an empty string
     }
 
-    if (count1 == 3) {
-      m4 = number_pressed;
+    if (count1 == 3) { //if count = 3 
+      m4 = number_pressed; // minutes 4 = number pressed
     }
 
-    if (count1 == 4) {
-      m5 = number_pressed;
-      m6 = (String(m4) + String(m5)).toInt();
-      m4 = "";
-      m5 = "";
+    if (count1 == 4) { // if count = 4
+      m5 = number_pressed; // m5 = numbers pressed
+      m6 = (String(m4) + String(m5)).toInt(); // adds the two number inputed as strings and convert them to an integer. 
+      m4 = ""; // reset m4 to an empty string
+      m5 = ""; // reset h5 to an empty string
     }
 
-    if (count1 == 5) {
-      s1 = number_pressed;
+    if (count1 == 5) { // if count = 5
+      s1 = number_pressed; // seconds 1 = number pressed. 
     }
 
-    if (count1 == 6) {
-      s2 = number_pressed;
-      s3 = (String(s1) + String(s2)).toInt();
-      s1 = "";
-      s2 = "";
-      hours = constrain(h6, 0, 23);
-      minutes = constrain(m6, 0, 59);
-      seconds = constrain(s3, 0, 59);
-      count1 = 0;
+    if (count1 == 6) { // if count = 6 
+      s2 = number_pressed; // seconds 2 = number pressed
+      s3 = (String(s1) + String(s2)).toInt(); // adds the two number inputed as strings and convert them to an integer. 
+      s1 = ""; // resets seconds to an empty string
+      s2 = ""; // resets seconds to an empty string
+      hours = constrain(h6, 0, 23); // constrain hours
+      minutes = constrain(m6, 0, 59); // constrain minutes
+      seconds = constrain(s3, 0, 59); // constrain seconds
+      count1 = 0; 
       Serial.println();
       Serial.print("set time hours : ");
       Serial.println(hours);
@@ -379,7 +379,7 @@ void loop() {
     delayMicroseconds(500);
     digitalWrite(53, LOW);
     delayMicroseconds(500);
-
+// Alarm goes off when time = alarm set time. 
     IRController(); // testing if this works?!
 
     if (mute == 1) {
